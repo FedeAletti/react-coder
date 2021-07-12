7ce6f278ef60c8ba534cc00b4f8f20c1d4a79d35
Federico Aletti <fedeale2000@gmail.com>
2021-07-12 19:35:29 -0300

    Correccion de links y clases

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,32 +5,32 @@ export const NavBar = () => {
         <>
         <nav className="navbar navbar-expand-xl navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand fw-bold" href="#">ShishaShop</a>
+                <a className="navbar-brand fw-bold" href="/#">ShishaShop</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic" aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse show m-auto" id="navbarBasic">
                     <ul className="navbar-nav me-auto mb-2 mb-xl-0">
                         <li className="nav-item">
-                            <a className="nav-link nav-link-p" href="#">Home</a>
+                            <a className="nav-link nav-link-p" href="/#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-p" href="#">Narguilas</a>
+                            <a className="nav-link nav-link-p" href="/#">Narguilas</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-p" href="#">Tabacos</a>
+                            <a className="nav-link nav-link-p" href="/#">Tabacos</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-p" href="#">Carbones</a>
+                            <a className="nav-link nav-link-p" href="/#">Carbones</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link nav-link-p dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link nav-link-p dropdown-toggle" href="/#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Accesorios
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">Mangueras</a></li>
-                                <li><a class="dropdown-item" href="#">Cabezales</a></li>
-                                <li><a class="dropdown-item" href="#">Otros</a></li>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a className="dropdown-item" href="/#">Mangueras</a></li>
+                                <li><a className="dropdown-item" href="/#">Cabezales</a></li>
+                                <li><a className="dropdown-item" href="/#">Otros</a></li>
                             </ul>
                             </li>
                     </ul>
