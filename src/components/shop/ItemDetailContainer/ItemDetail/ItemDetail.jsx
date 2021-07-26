3fb440da5051a7a3058a4bca4cb65cb56002aee8
Federico Aletti <fedeale2000@gmail.com>
2021-07-25 21:02:56 -0300

    ItemDetail

--- src/components/shop/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ src/components/shop/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -4,16 +4,16 @@ export const ItemDetail = ({item}) => {
     
     return (
         <>
-            <div className="height d-flex justify-content-center align-items-center">
+            <div className=" d-flex justify-content-center align-items-center shadow">
                 <div className="card p-3 w-100 ">
                     <div className="d-flex justify-content-between align-items-center container w-75 ">
                         <div className="mt-2">
                             <div className="mt-5">
-                                <h5 className="text-uppercase mb-0">{item.title}</h5>
-                                <h1 className="main-heading mt-0">{item.brand}</h1>
+                                <h4 className="main-heading mt-0">{item.brand}</h4>
+                                <h1 className="text-uppercase mb-0">{item.title}</h1>
                                 <h4 className="text-uppercase">${item.price}</h4>
                                 <hr />
-                                <h3>Descripción: </h3>
+                                <h3 >Descripción: </h3>
                                 <p>{ item.description }</p>
                             </div>
                         </div>
